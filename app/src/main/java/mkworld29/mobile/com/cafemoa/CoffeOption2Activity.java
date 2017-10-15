625928f32e474182a5a6f188ee84cfc7226f8baf
Eunseo Lee <ABClass98@naver.com>
2017-10-15 17:37:40 +0900

    modified

--- app/src/main/java/mkworld29/mobile/com/cafemoa/CoffeOption2Activity.java
+++ app/src/main/java/mkworld29/mobile/com/cafemoa/CoffeOption2Activity.java
@@ -4,11 +4,13 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
